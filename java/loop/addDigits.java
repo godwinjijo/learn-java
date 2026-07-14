8b5aed72a3e7d43f4e33259ec9111aa0050c908b
godwinjijo <112561534+godwinjijo@users.noreply.github.com>
2026-07-14 17:46:18 +0530

    problem - Sum of Digits in a Number

---
+++ java/loop/addDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class addDigits {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter the Number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        System.out.println(sum);
+    }
+}
